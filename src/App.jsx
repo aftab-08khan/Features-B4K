0e44e15523bbb8119c653a0ec4a4a746afb0997c
aftab-08khan <akkhanaftab08@gmail.com>
2026-07-07 15:59:19 +0400

    Issues Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import ModrenCard from "./pages/ModrenCard";
 import LegoConverter from "./pages/LegoConverter";
-import Game from './pages/Game'
 
 const App = () => {
   return (
@@ -12,7 +11,6 @@ const App = () => {
           <Route element={<Home />} path="/" index />
           <Route element={<ModrenCard/>} path="/modren-card"/>
           <Route element={<LegoConverter/>} path="/lego-convertor"/>
-          <Route element={<Game/>} path="/game"/>
 
 
         </Routes>
