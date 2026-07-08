5bb4cea2359d6908fd15e00fa48091174af902b1
aftab-08khan <akkhanaftab08@gmail.com>
2026-07-08 15:32:57 +0400

    New Page Added

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,18 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import ModrenCard from "./pages/ModrenCard";
 import LegoConverter from "./pages/LegoConverter";
+import ReportsDashboard from "./pages/ReportsDashboard";
 
 const App = () => {
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route element={<Home />} path="/" index />
-          <Route element={<ModrenCard/>} path="/modren-card"/>
-          <Route element={<LegoConverter/>} path="/lego-convertor"/>
-
-
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Home />} path="/" index />
+        <Route element={<ModrenCard />} path="/modren-card" />
+        <Route element={<LegoConverter />} path="/lego-convertor" />
+        <Route element={<ReportsDashboard/>} path="/reports"/>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
